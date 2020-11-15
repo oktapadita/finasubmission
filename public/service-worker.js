2bf4b94022be8a8049b733aa2940e6c14a8e1715
oktapadita <38067767+oktapadita@users.noreply.github.com>
2020-11-15 22:26:30 +0800

    worlbox

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,3 @@
-const CACHE_NAME = "serieaitaly";
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
  
 if (workbox)
@@ -29,13 +27,24 @@ else
     { url: 'https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2', revision: '1' },
     { url: '/icons/icon-72x72.png', revision: '1' },
     { url: '/icons/icon-96x96.png', revision: '1' },
-    { url: '/icons/icon-128x128.png', revision: '1' },
-    { url: '/icons/icon-144x144.png', revision: '1' },
-    { url: '/icons/icon-152x152.png', revision: '1' },
-    { url: '/icons/icon-192x192.png', revision: '1' },
-    { url: '/icons/icon-384x384.png', revision: '1' },
-    { url: '/icons/icon-512x512.png', revision: '1' },
-]);
+    { url: '/icons/icon-128x128.png', revision: '2' },
+    { url: '/icons/icon-144x144.png', revision: '2' },
+    { url: '/icons/icon-152x152.png', revision: '2' },
+    { url: '/icons/icon-192x192.png', revision: '2' },
+    { url: '/icons/icon-384x384.png', revision: '2' },
+    { url: '/icons/icon-512x512.png', revision: '2' },
+], {
+ignoreUrlParametersMatching: [/.*/]
+});
+
+workbox.routing.registerRoute(
+  new RegExp('(https://api.football-data.org/v2/)'),
+  workbox.strategies.staleWhileRevalidate(
+    {
+      cacheName: 'data-live'
+    }
+  )
+);
 
 workbox.routing.registerRoute(
   new RegExp('/pages/'),
@@ -45,50 +54,6 @@ workbox.routing.registerRoute(
     }
   )
 );
-
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", function(event) {
-  var base_url = "https://api.football-data.org/v2/";
-  const online = navigator.onLine;
-  if (event.request.url.indexOf(base_url) > -1 && online) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return fetch(event.request).then(function(response) {
-          cache.put(event.request.url, response.clone());
-          return response;
-        })
-      })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request, { ignoreSearch: true }).then(function(response) {
-        return response || fetch (event.request);
-      })
-    )
-  }
-});
-
-self.addEventListener("activate", function (event) {
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheName != CACHE_NAME) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
 
 self.addEventListener('push', function(event) {
   var body;
